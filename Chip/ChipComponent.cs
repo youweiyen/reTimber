c8225e09f6e63597d8cf68980389e5f249d0f1e0
youweiyen <wenyinyen@gmail.com>
2023-01-15 16:31:07 +0100

    solved joined curves get average

--- Chip/ChipComponent.cs
+++ Chip/ChipComponent.cs
@@ -3,6 +3,8 @@ using Grasshopper.Kernel;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace Chip
 {
@@ -21,12 +23,15 @@ namespace Chip
             "Category", "Subcategory")
         {
         }
+        //List<Curve> previewCurve = new List<Curve>();
 
         /// <summary>
         /// Registers all the input parameters for this component.
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddMeshParameter("ScanMesh", "ScanMesh", "ScanMesh", GH_ParamAccess.list);
+            pManager.AddMeshParameter("SectionSides", "SectionSides", "SectionSides", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -34,6 +39,8 @@ namespace Chip
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddCurveParameter("CenterCurve","CenterCurve", "CenterCurve", GH_ParamAccess.list);
+            pManager.AddGeometryParameter("section","section","section",GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -43,7 +50,122 @@ namespace Chip
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            List<Mesh> ScannedMeshes = new List<Mesh>();
+            List<Mesh> SectionSides = new List<Mesh>();
+            DA.GetDataList(0, ScannedMeshes);
+            DA.GetDataList(1, SectionSides);
+
+            List<Point3d> contourPoints = new List<Point3d>();
+            foreach(Mesh ss in SectionSides) 
+            {
+                BoundingBox bbox = ss.GetBoundingBox(false);
+                Point3d p = bbox.Center;
+                //Brep bbrep = Brep.CreateFromBox(bbox);
+                //Brep sectionFace = bbrep.Faces.OrderByDescending(f => AreaMassProperties.Compute(f, true, false, false, false).Area).First().ToBrep();
+                //Point3d p = AreaMassProperties.Compute(sectionFace,false,false,false,false).Centroid;
+                contourPoints.Add(p);
+            }
+            Vector3d sectionVector = new Vector3d(contourPoints[1].X - contourPoints[0].X, contourPoints[1].Y - contourPoints[0].Y, contourPoints[1].Z - contourPoints[0].Z);
+            sectionVector.Unitize();
+            Transform movealongsection = Transform.Translation(sectionVector);
+            Point3d sectionstart = contourPoints[0];
+            sectionstart.Transform(movealongsection);
+            Mesh JoinMesh = new Mesh();
+            foreach(Mesh sm in ScannedMeshes)
+            {
+                JoinMesh.Append(sm);
+            }
+            double contourDist = NumericExtensions.FromMeter(0.02);
+            IEnumerable<Curve> contourcurves = Mesh.CreateContourCurves(JoinMesh, sectionstart, contourPoints[1], contourDist, 0.01).ToList();
+            List<Curve> contourCrv = Curve.JoinCurves(contourcurves, contourDist*0.1, false).ToList();
+            //List<Curve> contourCrv = new List<Curve>();
+            //for(int j = 0; j < joined.Count; j++)
+            //{
+            //    foreach(Curve crv in joined)
+            //    {
+            //        if (GeometryBase.GeometryEquals(crv, joined[j])== false)
+            //        {
+            //            continue;
+            //        }
+            //        else { contourCrv.Add(joined[j]); }
+            //    }
+
+            //}
+            //List<Curve> contourCrv = new List<Curve>();
+            //for (int i = 0; i < joinedcontourCrv.Count; i++)
+            //{
+            //    if (i == 0) { contourCrv.Add(joinedcontourCrv[i]); }
+            //    else if (i == joinedcontourCrv.Count - 1) { continue; }
+            //    else
+            //    {
+            //        joinedcontourCrv[i - 1].ClosestPoints(joinedcontourCrv[i], out Point3d pointoncurve1, out Point3d pointoncurve2);
+            //        if (pointoncurve1.DistanceTo(pointoncurve2) < contourDist * 2)
+            //        {
+            //            contourCrv.Add(joinedcontourCrv[i]);
+            //        }
+            //    }
+            //}
+            List<Point3d> centers = new List<Point3d>();
+            foreach(Curve crv in contourCrv)
+            {
+                crv.DivideByCount(20, true, out Point3d[] cP);
+                List<Point3d> curvePoints = cP.ToList();
+                double averageX = curvePoints.Average(bp => bp.X);
+                double averageY = curvePoints.Average(bp => bp.Y);
+                double averageZ = curvePoints.Average(bp => bp.Z);
+                Point3d average = new Point3d(averageX, averageY, averageZ);
+                centers.Add(average);
+            }
+            List<Point3d> orderedcenter = centers.OrderByDescending(cen => cen.DistanceTo(contourPoints[1])).ToList().ToList();
+            List<Polyline> polylines = new List<Polyline>();
+            for(int i= 0; i< orderedcenter.Count; i++)
+            {
+                Polyline pline = new Polyline();
+                if(i == 0)
+                {
+                    pline.Add(contourPoints[0]);
+                    pline.Add(orderedcenter[i]);
+                }
+                else if (i == orderedcenter.Count-1) 
+                {
+                    pline.Add(orderedcenter[i]);
+                    pline.Add(contourPoints[1]);
+                    polylines.Add(pline);
+                }
+
+                else
+                {
+                    pline.Add(orderedcenter[i]);
+                    pline.Add(orderedcenter[i + 1]);
+                    polylines.Add(pline);
+                }
+
+            }
+
+            //previewCurve.AddRange(contourCrv);
+
+
+            DA.SetDataList(0, polylines);
+            DA.SetDataList(1, contourCrv);
+
         }
+
+        //preview
+        //public override void DrawViewportMeshes(IGH_PreviewArgs args)
+        //{
+        //    Rhino.Display.DisplayMaterial dm = new Rhino.Display.DisplayMaterial(System.Drawing.Color.Blue);
+        //    if (previewCurve != null)
+        //    {
+        //        foreach (var cr in previewCurve)
+        //        {
+        //            if (cr != null)
+        //                args.Display.DrawCurve(cr, System.Drawing.Color.AliceBlue, 2);
+        //        }
+
+
+        //    }
+        //}
+
 
         /// <summary>
         /// Provides an Icon for every component that will be visible in the User Interface.
@@ -59,5 +181,35 @@ namespace Chip
         /// that use the old ID will partially fail during loading.
         /// </summary>
         public override Guid ComponentGuid => new Guid("c4e36432-eeac-42f7-8682-f08a6f8cf886");
+    }
+    public static class NumericExtensions
+    {
+        public static double FromMeter(this double length)
+        {
+            return length * GetConversionFactor();
+        }
+
+
+        public static double GetConversionFactor()
+        {
+
+            switch (Rhino.RhinoDoc.ActiveDoc.ModelUnitSystem)
+            {
+                case Rhino.UnitSystem.Meters:
+                    return 1.0;
+
+                case Rhino.UnitSystem.Millimeters:
+                    return 1000.0;
+
+                case Rhino.UnitSystem.Centimeters:
+                    return 100.0;
+
+                case Rhino.UnitSystem.Feet:
+                    return 304.8 / 1000.0;
+
+                default:
+                    throw new Exception("unknown units");
+            }
+        }
     }
 }
