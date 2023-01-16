a4fd882afc0448d90551b67238deaea7b6601035
youweiyen <wenyinyen@gmail.com>
2023-01-16 15:19:51 +0100

    made the bounding box aligned to object

--- Chip/TimberJoint.cs
+++ Chip/TimberJoint.cs
@@ -23,6 +23,9 @@ namespace Chip
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddMeshParameter("SegmentMeshes", "SegmentMeshes", "SegmentMeshes", GH_ParamAccess.list);
+            pManager.AddCurveParameter("CenterAxis", "CenterAxis", "CenterAxis", GH_ParamAccess.item);
+            
         }
 
         /// <summary>
@@ -30,6 +33,8 @@ namespace Chip
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddMeshParameter("JointMesh","JointMesh","JointMesh", GH_ParamAccess.list);
+            pManager.AddBrepParameter("brep", "brep", "brep", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -38,6 +43,31 @@ namespace Chip
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            List<Mesh> meshFaces = new List<Mesh>();
+            Curve centerAxis = null;
+            DA.GetDataList(0, meshFaces);
+            DA.GetData(1, ref centerAxis);
+
+            //Get aligned object bounding box
+            Point3d startpoint = centerAxis.PointAtStart;
+            Point3d endpoint = centerAxis.PointAtEnd;
+            Vector3d centerVector = new Vector3d(startpoint.X - endpoint.X, startpoint.Y - endpoint.Y, startpoint.Z - endpoint.Z);
+            Plane boxPlane = new Plane(startpoint, centerVector);
+            Mesh joinedmesh = new Mesh();
+            foreach (Mesh mesh in meshFaces) { joinedmesh.Append(mesh); }
+            //plane aligned to origin. transform brep to aligned object
+            BoundingBox boundingBox = joinedmesh.GetBoundingBox(boxPlane);
+            Plane originplane = new Plane(new Point3d(0, 0, 0), new Vector3d(0, 0, 1));
+            Transform orient = Transform.PlaneToPlane(originplane, boxPlane);
+
+            Brep brep = Brep.CreateFromBox(boundingBox);
+            brep.Transform(orient);
+
+            //get the surfaces that are the same normal to each faces of the brep
+            
+            
+
+            DA.SetData(1, brep);
         }
 
         /// <summary>
